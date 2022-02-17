d19c2a68bc5b4d7d1ff9d272a080458202d59fc5
mdrashadul-rimon <mdrashadul.rimon@gmail.com>
2022-02-17 14:59:33 +0600

    basic code updated full

--- js/script.js
+++ js/script.js
@@ -16,20 +16,37 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     let rentInput = document.getElementById('rent-input');
     let clothesInput = document.getElementById('clothes-input');
 
-    let income = parseFloat(incomeInput.value);
-    let totalExpenses = parseFloat(foodInput.value) + parseFloat(rentInput.value) + parseFloat(clothesInput.value);
     let balance = document.getElementById('balance');
 
+    let income = parseFloat(incomeInput.value);
+    let totalExpenses = parseFloat(foodInput.value) + parseFloat(rentInput.value) + parseFloat(clothesInput.value);
+
     //expenses
-    if (income >= totalExpenses || income > document.getElementById('saving-percent').value ) {
+    if (income >= totalExpenses) {
         document.getElementById('total-expenses').innerText = totalExpenses;
         let showBalance = income - totalExpenses;
         balance.innerText = showBalance;
     }
-    else {
-        validation();
+
+    // else {
+    //     validation();
+    // }
+});
+
+document.getElementById('saving-btn').addEventListener('click', function () {
+    let incomeInput = document.getElementById('income-input');
+    let income = parseFloat(incomeInput.value);
+
+    let savingInput = document.getElementById('saving-input');
+    let savings = parseFloat(savingInput.value);
+    if (income > savings) {
+        let savingsAmount = (income/100) * savings;
+        document.getElementById('savings-amount').innerText = savingsAmount;
+        
+        let balance = parseFloat(document.getElementById('balance').innerText);
+        
+        let remainingBalance = balance - savingsAmount;
+        document.getElementById('remaining-balance').innerText = remainingBalance;
+        
     }
-
-
-
 });
