a9ea1ea3fc5a7e35183108ce00ab5fbbcfafd920
mdrashadul-rimon <mdrashadul.rimon@gmail.com>
2022-02-17 21:41:37 +0600

    toughly finished all the parts of validation and access function

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
 function validation() {
     let validationError = document.getElementById('validate');
     let incomeInput = getIncomeInput();
-    if (isNaN(incomeInput)) {
-        validationError.textContent = "Please enter a valid number";
+    let foodInput = getFoodInput();
+    let rentInput = getRentInput();
+    let clothesInput = getClothesInput();
+    let balance = getFromBalance();
+
+    if (isNaN(incomeInput) || isNaN(foodInput) || isNaN(rentInput) || isNaN(clothesInput)) {
+        validationError.textContent = "Please enter a valid number not string";
         validationError.style.color = "red";
-    } else if (incomeInput < 0) {
-        validationError.textContent = "Do not give negative number";
+    } else {
+        validationError.textContent = "STOP! Should not be negative, Stay positive always.";
         validationError.style.color = "blue";
     }
 }
+
 //income input
 function getIncomeInput() {
     let incomeInput = document.getElementById('income-input');
     let income = parseFloat(incomeInput.value);
     return income;
+}
+//food input
+function getFoodInput() {
+    let foodInput = document.getElementById('food-input');
+    let food = parseFloat(foodInput.value);
+    return food;
+}
+function getRentInput() {
+    let rentInput = document.getElementById('rent-input');
+    let rent = parseFloat(rentInput.value);
+    return rent;
+}
+function getClothesInput() {
+    let clothesInput = document.getElementById('clothes-input');
+    let clothes = parseFloat(clothesInput.value);
+    return clothes;
 }
 //balance to show
 function postToBalance(num) {
@@ -33,40 +55,49 @@ function getFromBalance() {
 document.getElementById('calculate-btn').addEventListener('click', function () {
 
     let income = getIncomeInput(); //call income input function
+    let food = getFoodInput();
+    let rent = getRentInput();
+    let clothes = getClothesInput();
 
-    //getting the inputs from fields
-    let foodInput = document.getElementById('food-input');
-    let rentInput = document.getElementById('rent-input');
-    let clothesInput = document.getElementById('clothes-input');
-
-    // adding all the values of expenses
-    let totalExpenses = parseFloat(foodInput.value) + parseFloat(rentInput.value) + parseFloat(clothesInput.value);
-
-    //expenses check validation
-    if (income >= totalExpenses) {
-        document.getElementById('total-expenses').innerText = totalExpenses;
-        let showBalance = income - totalExpenses;
-        balance = showBalance;
-        postToBalance(balance); //call balance function with sending arguments to parameter to show output
-    }
-    else {
+    if ((income < 0) || (food < 0) || (rent) < 0 || (clothes < 0)) {
         validation();
+    }
+    //expenses check validation
+    else {
+        // adding all the values of expenses
+        let totalExpenses = food + rent + clothes;
+        if (income >= totalExpenses) {
+            document.getElementById('total-expenses').innerText = totalExpenses;
+            let showBalance = income - totalExpenses;
+            balance = showBalance;
+            postToBalance(balance); //call balance function with sending arguments to parameter to show output
+        }
+        else {
+            validation();
+        }
     }
 });
 
+//Savings Amount part
 document.getElementById('saving-btn').addEventListener('click', function () {
 
     let income = getIncomeInput();//call income input function
-    let balance = getFromBalance();
+    let balance = getFromBalance(); //call balance function
 
     let savingInput = document.getElementById('saving-input');
     let savings = parseFloat(savingInput.value);
-    if (income > savings) {
-        let savingsAmount = (income / 100) * savings;
+
+    // calculation of savings
+    let savingsAmount = (income / 100) * savings;
+    //validation checking and calling function of validation
+    if ((income > savings) && (savings < balance) && (savingsAmount < balance)) {
         document.getElementById('savings-amount').innerText = savingsAmount.toFixed(2);
-        
+
         let remainingBalance = balance - savingsAmount;
         // console.log(remainingBalance);
-        document.getElementById('remaining-balance').innerText = remainingBalance;
+        document.getElementById('remaining-balance').innerText = remainingBalance.toFixed(2);
+    }
+    else {
+        validation();
     }
 });
